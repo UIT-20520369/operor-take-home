c98c76da77d2f0f948da278fdb5ac86ef6e84346
khanhan2k2eliai <84655671+anbuibz54@users.noreply.github.com>
2024-06-12 21:02:48 +0700

    build fix

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import "reflect-metadata";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  // const config = new DocumentBuilder()
-  //   .setTitle('Operor API')
-  //   .setDescription('API description')
-  //   .setVersion('1.0')
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
+  const config = new DocumentBuilder()
+    .setTitle('Operor API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3001);
 }
 bootstrap();
