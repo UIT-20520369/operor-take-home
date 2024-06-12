29729697441eb1682f762f311bd66cb86d1c45dc
khanhan2k2eliai <84655671+anbuibz54@users.noreply.github.com>
2024-06-12 21:31:22 +0700

    try again

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import "reflect-metadata";
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{cors:true});
   app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Operor API')
